97bf14e575efa61475f7feadccda355e95c6a66d
Shashika dilshan <agnidilshan384@gmail.com>
2025-10-31 11:25:07 +0530

    Update auto.js

--- plugins/auto.js
+++ plugins/auto.js
@@ -1,76 +1,59 @@
 //𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐛𝐲 𝐬𝐡𝐚𝐬𝐡𝐢𝐤𝐚 𝐝𝐢𝐥𝐬𝐡𝐚𝐧//
 const fs = require('fs');
 const path = require('path');
-const {readEnv} = require('../lib/database')
-const {cmd , commands} = require('../command')
+const { readEnv } = require('../lib/database');
+const { cmd } = require('../command');
 
-//auto_voice
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
+// auto_voice
+cmd({ on: "body" }, async (conn, mek, m, { from, body, isOwner }) => {
+    const config = await readEnv(); // DB එකෙන් load කරනවා
     const filePath = path.join(__dirname, '../sd-data/auto-voice.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            
             if (config.AUTO_VOICE === 'true') {
-                //if (isOwner) return;        
                 await conn.sendPresenceUpdate('recording', from);
                 await conn.sendMessage(from, { audio: { url: data[text] }, mimetype: 'audio/mpeg', ptt: true }, { quoted: mek });
             }
         }
-    }                
+    }
 });
 
-//auto sticker 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
+// auto_sticker
+cmd({ on: "body" }, async (conn, mek, m, { from, body, isOwner }) => {
+    const config = await readEnv();
     const filePath = path.join(__dirname, '../sd-data/auto-stickers.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            
             if (config.AUTO_STICKER === 'true') {
-                //if (isOwner) return;        
-                await conn.sendMessage(from,{sticker: { url : data[text]},package: 'Malvin King'},{ quoted: mek })   
-            
+                await conn.sendMessage(from, { sticker: { url: data[text] }, package: 'Malvin King' }, { quoted: mek });
             }
         }
-    }                
+    }
 });
 
-//auto reply 
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {
+// auto_reply
+cmd({ on: "body" }, async (conn, mek, m, { from, body, isOwner }) => {
+    const config = await readEnv();
     const filePath = path.join(__dirname, '../sd-data/auto-reply.json');
     const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
     for (const text in data) {
         if (body.toLowerCase() === text.toLowerCase()) {
-            
             if (config.AUTO_REPLY === 'true') {
-                //if (isOwner) return;        
-                await m.reply(data[text])
-            
+                await m.reply(data[text]);
             }
         }
-    }                
+    }
 });
 
-
-//fake recording
-cmd({
-  on: "body"
-},    
-async (conn, mek, m, { from, body, isOwner }) => {       
- if (config.FAKE_RECORDING === 'true') {
-                await conn.sendPresenceUpdate('recording', from);
-            }
-         } 
-   );
-
-    
+// fake_recording
+cmd({ on: "body" }, async (conn, mek, m, { from, body, isOwner }) => {
+    const config = await readEnv();
+    if (config.FAKE_RECORDING === 'true') {
+        await conn.sendPresenceUpdate('recording', from);
+    }
+});
